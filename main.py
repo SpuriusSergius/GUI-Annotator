d9d0c2acb7e9a55550a2b9a23c037d379a7a0021
Steven Jia <szjia2010@gmail.com>
2023-09-04 20:01:09 -0400

    Added Comments Added comments to describe and explain code

--- main.py
+++ main.py
@@ -2,6 +2,7 @@ import xml.etree.ElementTree as ET
 from PIL import Image, ImageDraw, ImageColor
 import re
 
+# function that creates a list of leaf nodes given a root node
 def get_leaves(node, leaves_list):
   children = node.findall("*")
   if not children:
@@ -15,22 +16,33 @@ def get_leaves(node, leaves_list):
 # Programming-Assignment-Data\com.apalon.ringtones Programming-Assignment-Data\com.dropbox.android Programming-Assignment-Data\com.giphy.messenger-1 Programming-Assignment-Data\com.giphy.messenger-2 Programming-Assignment-Data\com.google.android.apps.transalte Programming-Assignment-Data\com.pandora.android Programming-Assignment-Data\com.yelp.android
 file_list = input("Provide the path of all png-xml file pairs, without file extensions, to be annotated separated by a single whitespace (ex: Programming-Assignment-Data\com.apalon.ringtones)\n")
 file_list = file_list.split(" ")
-print(file_list)
 
+# iterate through all inputted files
 for file in file_list:
+
+  # parse the xml file and get the root element
   tree = ET.parse(file + ".xml")
   root = tree.getroot()
-  print("Root is",root)
+
+  # find the leaf elements of the xml file
   leaves_list = []
   get_leaves(root, leaves_list)
-  print(leaves_list)
+
+  # open corresponding PNG
   with Image.open(file + ".png") as im:
-    print(im.format, im.size, im.mode)
     draw = ImageDraw.Draw(im)
+
+    # for each leaf element, use the element's bounds valye to draw a rectangle on the png given.
     for element in leaves_list:
-      print(element.attrib['bounds'])
+
+      # bounds is a string, convert into a lust of integers
       bounds = re.findall('[0-9]+', element.attrib['bounds'])
       bounds = list(map(int, bounds))
-      print(bounds)
+
+      # draw the rectangle
       draw.rectangle(bounds, fill = None, outline = ImageColor.getrgb("yellow"), width=5)
+
+    # save annotated PNG
     im.save(file + ".annotated.png")
+
+print("Annotations complete. Annotated PNGs can be found in the same location as input with name \"[original_filename].annotated.png\"")
